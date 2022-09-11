6dd0e3004f60b50fbe1af3fcc11d0350f1b77ee3
Özgür Yalçın <ozguryalcin@outlook.com>
2022-09-11 17:03:52 +0300

    Update netgsm.go

--- src/netgsm.go
+++ src/netgsm.go
@@ -7,59 +7,69 @@ import (
 	"strings"
 )
 
-type Config struct {
-	ApiUrl       string
-	SmsCompany   string
-	SmsMsgHeader string
-	SmsUserCode  string
-	SmsPassword  string
-}
-
 type API struct {
-	Config Config
+	Endpoint string
 }
 
 type Request struct {
-	XMLName  xml.Name `xml:"xml,omitempty"`
-	MainBody struct {
-		Header struct {
-			Company   string `xml:"company,omitempty"`
-			UserCode  string `xml:"usercode,omitempty"`
-			Password  string `xml:"password,omitempty"`
-			StartDate string `xml:"startdate,omitempty"`
-			StopDate  string `xml:"stopdate,omitempty"`
-			Type      string `xml:"type,omitempty"`
-			MsgHeader string `xml:"msgheader,omitempty"`
-		} `xml:"header,omitempty"`
-		Body struct {
-			Msg string `xml:"msg,omitempty"`
-			No  string `xml:"no,omitempty"`
-		} `xml:"body,omitempty"`
-	} `xml:"mainbody,omitempty"`
+	XMLName xml.Name `xml:"mainbody,omitempty"`
+	Header  struct {
+		Company   string `xml:"company,omitempty"`
+		UserCode  string `xml:"usercode,omitempty"`
+		Password  string `xml:"password,omitempty"`
+		StartDate string `xml:"startdate,omitempty"`
+		StopDate  string `xml:"stopdate,omitempty"`
+		Type      string `xml:"type,omitempty"`
+		MsgHeader string `xml:"msgheader,omitempty"`
+	} `xml:"header,omitempty"`
+	Body struct {
+		Msg string `xml:"msg,omitempty"`
+		No  string `xml:"no,omitempty"`
+	} `xml:"body,omitempty"`
 }
 
 type Response struct {
 	XMLName xml.Name `xml:"xml,omitempty"`
-	XML     struct {
-		Main struct {
-			Code  string `xml:"code,omitempty"`
-			JobID string `xml:"jobID,omitempty"`
-		} `xml:"main,omitempty"`
-	} `xml:"xml,omitempty"`
+	Main    struct {
+		Code  string `xml:"code,omitempty"`
+		JobID string `xml:"jobID,omitempty"`
+		Error string `xml:"error,omitempty"`
+	} `xml:"main,omitempty"`
 }
 
-func (api *API) Sms(request Request) (res Response) {
-	request.MainBody.Header.Company = api.Config.SmsCompany
-	request.MainBody.Header.MsgHeader = api.Config.SmsMsgHeader
-	request.MainBody.Header.UserCode = api.Config.SmsUserCode
-	request.MainBody.Header.Password = api.Config.SmsPassword
-	request.MainBody.Header.Type = "1:n"
-	request.MainBody.Body.Msg = "<![CDATA[" + request.MainBody.Body.Msg + " - ]]>"
+func Api(header, usercode, password string) (*API, *Request) {
+	api := new(API)
+	api.Endpoint = "https://api.netgsm.com.tr"
+	request := new(Request)
+	request.Header.Company = "NETGSM"
+	request.Header.MsgHeader = header
+	request.Header.UserCode = usercode
+	request.Header.Password = password
+	request.Header.Type = "1:n"
+	return api, request
+}
+
+func (api *API) Sms(request *Request) bool {
+	request.Body.Msg = "<![CDATA[" + request.Body.Msg + " - ]]>"
+	postdata, err := xml.Marshal(request)
+	if err != nil {
+		return false
+	}
+	response, err := http.Post(api.Endpoint+"/sms/send/xml", "text/xml", strings.NewReader(xml.Header+html.UnescapeString(string(postdata))))
+	if err != nil {
+		return false
+	}
+	defer response.Body.Close()
+	return true
+}
+
+func (api *API) Otp(request *Request) (res Response) {
+	request.Body.Msg = "<![CDATA[" + request.Body.Msg + " - ]]>"
 	postdata, err := xml.Marshal(request)
 	if err != nil {
 		return res
 	}
-	response, err := http.Post(api.Config.ApiUrl, "text/xml; charset=utf-8", strings.NewReader(xml.Header+html.UnescapeString(string(postdata))))
+	response, err := http.Post(api.Endpoint+"/sms/send/otp", "text/xml", strings.NewReader(xml.Header+html.UnescapeString(string(postdata))))
 	if err != nil {
 		return res
 	}
